56f2bb0c42bb7252199b73d2e4e10a43e6c0f0c1
YenNhi-K <nhikuteo177@gmail.com>
2024-10-13 15:22:57 +0700

    remove header axios

--- src/api/category/index.js
+++ src/api/category/index.js
@@ -3,21 +3,13 @@ import axiosInstance from "../../utils/axios";
 const token = localStorage.getItem('token')
 const handleGetAllCategories = (page, limit, sortBy, order, name) => {
   console.log({page, limit, sortBy, order, name})
-  return axiosInstance.get(`/admin/categories?page=${page}&limit=${limit}${sortBy ? `&sortBy=${sortBy}&order=${order}` : ""}&name=${name}`,{
-    headers: {
-      Authorization: `Bearer ${token}`
-    }
-  });
+  return axiosInstance.get(`/admin/categories?page=${page}&limit=${limit}${sortBy ? `&sortBy=${sortBy}&order=${order}` : ""}&name=${name}`);
 };
 
 const handleCreateCategory = (name, description) => {
   return axiosInstance.post("/admin/categories", {
     name,
     description,
-  },{
-    headers: {
-      Authorization: `Bearer ${token}`
-    }
   });
 };
 
@@ -25,19 +17,11 @@ const handleEditCategory = (name, description) => {
   return axiosInstance.put("/admin/categories", {
     name,
     description,
-  },{
-    headers: {
-      Authorization: `Bearer ${token}`
-    }
   });
 };
 
 const handleDeleteCategory = (categoryId) => {
-  return axiosInstance.delete(`/admin/categories/${categoryId}`, {
-    headers: {
-      Authorization: `Bearer ${token}`
-    }
-  });
+  return axiosInstance.delete(`/admin/categories/${categoryId}`);
 };
 
 
