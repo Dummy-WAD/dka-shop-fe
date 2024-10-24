92c2aefa1398ea2a859dec27cb1864b486b96659
hongdiem <mingukmanse673@gmail.com>
2024-10-24 11:23:33 +0700

    DEK-135 Fix css for home page

--- src/layout/LayoutUser/LayoutUser.jsx
+++ src/layout/LayoutUser/LayoutUser.jsx
@@ -1,21 +1,17 @@
-import { Outlet } from "react-router-dom";
+import { Outlet, useLocation } from "react-router-dom";
 import Footer from "./Footer/Footer";
 import NavbarUser from "./NavbarUser/NavbarUser";
 
 const LayoutUser = () => {
+  const location = useLocation();
   return (
     <div>
       <NavbarUser />
-      <div className="banner" style={{ width: "100%", height: "600px", overflow: "hidden"}}>
-        <img
-          src="../../../public/bannerr.png"
-          alt="Banner"
-          style={{ width: "100%", height: "100%", objectFit: "cover" }}
-        />
-      </div>
-      <div style={{ padding: "30px 50px", backgroundColor: "#f3f4f6" }}>
-        <div style={{ maxWidth: "1200px", margin: "0 auto" }}>
-          <Outlet />
+      <div style={{ background: location.pathname === '/' ? '#f3f4f6' : "none" }}>
+        <div style={{ paddingBottom: "30px" }}>
+          <div style={{ minWidth: '1200px', margin: "0 auto" }}>
+            <Outlet />
+          </div>
         </div>
       </div>
       <Footer />
--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,48 +1,102 @@
-import { Button } from "@mui/material";
 import CategoryButton from "../../components/CategoryButton/CategoryButton";
 import { AllArrowIcon } from "../../icon/Icon";
 import classes from "./Home.module.css";
+import CardProduct from "../../components/CardProduct/CardProduct";
+import { useEffect, useState } from "react";
+import { getBestSellerProductsForCustomer } from "../../api/product";
+import { getBestSellerCategoriesForCustomer } from "../../api/category";
+import { useNavigate } from "react-router-dom"
 
 function Home() {
+  const [listProducts, setListProducts] = useState([]);
+  const [listCategories, setListCategories] = useState([]);
+  const [currentCategoryId, setCurrentCategoryId] = useState(null);
+  const navigate = useNavigate();
+
+  const fetchProductsAndCategories = async () => {
+    try {
+      const products = await getBestSellerProductsForCustomer({});
+      setListProducts(products);
+
+      const categories = await getBestSellerCategoriesForCustomer({});
+      setListCategories(categories);
+    } catch (err) {
+      console.error(err);
+    }
+  };
+
+  useEffect(() => {
+    fetchProductsAndCategories();
+  }, []);
+
+  const handleClickShowAllProducts = () => {
+    navigate(`/search`);
+    window.scrollTo(0, 0);
+  };
+
+  const handleFilterProductsByCategory = async (categoryId) => {
+    const newCategoryId = categoryId === currentCategoryId ? null : categoryId;
+  
+    const productsByCategory = await getBestSellerProductsForCustomer(
+      newCategoryId ? { categoryId: newCategoryId } : {}
+    );
+  
+    setListProducts(productsByCategory);
+    setCurrentCategoryId(newCategoryId);
+  };
+
   return (
-    <div className="wrapper" style={{ minHeight: "60vh" }}>
-      {/* Categories section */}
-      <div className={classes.categories}>
-        <div className={classes.categoriesHeader}>
-          <h1 style={{ padding: "20px 0 " }}>Categories</h1>
-        </div>
-        <div className={classes.categoriesBody}>
-          <CategoryButton>Category 1</CategoryButton>
-          <CategoryButton>Category 2</CategoryButton>
-          <CategoryButton>Category 3</CategoryButton>
-          <CategoryButton>Category 4</CategoryButton>
-          <CategoryButton>Category 5</CategoryButton>
-          <CategoryButton>Category 6</CategoryButton>
-        </div>
+    <>
+      <div className="banner" style={{ minWidth: "1200px", overflow: "hidden", margin: "0 auto" }}>
+        <img
+          src="../../../public/banner.png"
+          alt="Banner"
+          style={{ width: "100%", height: "100%", objectFit: "cover" }}
+        />
       </div>
-      {/* Best seller section */}
-      <div className={classes.bestSellerProduct}>
-        <div className={classes.bestSellerHeader}>
-          <div>
-            <h1>Best seller</h1>
-            <div>Exciting products at irresistible prices</div>
+      <div className="wrapper" style={{ minHeight: "60vh", margin: "30px 80px", marginBottom: 0 }}>
+        <div className={classes.categories}>
+          <div className={classes.categoriesHeader}>
+            <h2 style={{ padding: "20px 0 " }}>Categories</h2>
           </div>
-          <div>
-            <CategoryButton className={classes.outlined} >
-              {" All products"}
-              <AllArrowIcon />
+          <div className={classes.categoriesBody}>
+            {
+              listCategories.length > 0 ? listCategories.map((category, index) =>
+                <div key={index}>
+                  <CategoryButton className={`${currentCategoryId === category.id ? classes.borderActiveBtn : ''}`} onClick={() => handleFilterProductsByCategory(category.id)}>{category?.name || ""}</CategoryButton>
+                </div>
+              ) : <div style={{ display: "flex", justifyContent: "center", width: "100%" }}>No result found</div>
+            }
+          </div>
+        </div>
+        <div className={classes.bestSellerProduct}>
+          <div className={classes.bestSellerHeader}>
+            <div>
+              <h2>Best seller</h2>
+              <div style={{ color: "#9CA3AF", fontSize: "16px" }}>Exciting products at irresistible prices!</div>
+            </div>
+            <div>
+              <CategoryButton className={classes.allProductBtn} onClick={handleClickShowAllProducts}>
+                {" All products"}
+                <AllArrowIcon />
+              </CategoryButton>
+            </div>
+          </div>
+          <div className={classes.bestSellerBody}>
+            <div className={classes.listProduct}>
+              {listProducts?.length > 0 ? listProducts.map((item, index) => (
+                <CardProduct key={index} product={{ ...item, primaryImageUrl: item.imageUrl }} />
+              )) : <div style={{ display: "flex", justifyContent: "center", width: "100%" }}>No result found</div>}
+            </div>
+          </div>
+          <div style={{ display: "flex", justifyContent: "center" }}>
+            <CategoryButton className={classes.moreProductBtn} onClick={handleClickShowAllProducts}>
+              { "All products" }
             </CategoryButton>
           </div>
         </div>
-        <div className={classes.bestSellerBody}>All best seller product</div>
-        {/* More product button */}
-        <div style={{ display: "flex", justifyContent: "center"}}>
-          <CategoryButton className={classes.moreProductBtn} >
-            {"All products"}
-          </CategoryButton>
-        </div>
       </div>
-    </div>
+    </>
   );
 }
 
