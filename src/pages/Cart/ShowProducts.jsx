70b8a4facbf7daf249e77a85bada6593feec8814
nguyenthuchoang <nguyenthuchoang17112003@gmail.com>
2024-11-02 20:51:33 +0700

    [DEK-186]:Add modal show message when quantity out of stock

--- src/pages/Cart/ShowProducts.jsx
+++ src/pages/Cart/ShowProducts.jsx
@@ -10,6 +10,7 @@ import {
 } from "../../api/cart";
 import { toast } from "react-toastify";
 import { Link } from "react-router-dom";
+import { Modal, Box, Typography, Button } from "@mui/material";
 
 const DeliveryOptions = ({ selectedOption, setSelectedOption }) => {
   const options = [
@@ -58,18 +59,18 @@ const TotalCost = ({ totalCost, deliveryCost }) => {
     <div className={css.containerTotalCost}>
       <div className={css.costRow}>
         <span className={css.label}>Total product cost</span>
-        <span className={css.amount}>{totalCost.toFixed(2)}₫</span>
+        <span className={css.amount}>{totalCost.toFixed(2)}$</span>
       </div>
       <hr className={css.divider} />
       <div className={css.costRow}>
         <span className={css.label}>Delivery cost</span>
-        <span className={css.amount}>{deliveryCost.toFixed(2)}₫</span>
+        <span className={css.amount}>{deliveryCost.toFixed(2)}$</span>
       </div>
       <hr className={css.divider} />
       <div className={css.totalRow}>
         <span className={css.totalLabel}>Total payable amount</span>
         <span className={css.totalAmount}>
-          {(totalCost + deliveryCost).toFixed(2)}₫
+          {(totalCost + deliveryCost).toFixed(2)}$
         </span>
       </div>
       <button className={css.checkoutButton}>Check out</button>
@@ -88,7 +89,6 @@ const CartItem = ({
   listItemChecked,
   handleRemoveProduct,
   handleChangeQuantityProduct,
-  errorQuantity,
 }) => {
   return (
     <div className={css.cartItemContainer}>
@@ -170,9 +170,6 @@ const CartItem = ({
             ${item.totalPrice.toFixed(2)}
           </div>
         </div>
-        {errorQuantity && (
-          <div className={css.errorText}>Quantity exceeds available stock</div>
-        )}
       </div>
     </div>
   );
@@ -195,7 +192,6 @@ CartItem.propTypes = {
   listItemChecked: PropTypes.array.isRequired,
   handleRemoveProduct: PropTypes.func.isRequired,
   handleChangeQuantityProduct: PropTypes.func.isRequired,
-  errorQuantity: PropTypes.bool,
 };
 
 const ShowProduct = () => {
@@ -207,7 +203,7 @@ const ShowProduct = () => {
   const [selectedOption, setSelectedOption] = useState("free");
   const [deliveryCost, setDeliveryCost] = useState(0);
   const [listItemChecked, setListItemChecked] = useState([]);
-  const [errorQuantity, setErrorQuantity] = useState({});
+  const [openModal, setOpenModal] = useState(false);
 
   const onCheckItem = (id, price) => {
     const index = listItemChecked.indexOf(id);
@@ -223,13 +219,30 @@ const ShowProduct = () => {
   const fetchProducts = async () => {
     try {
       const response = await getAllProductsInCart();
-      setProducts(
-        response.results.map((item) => {
-          return {
-            ...item,
-          };
-        })
-      );
+      if (response) {
+        const cartItemsOutOfStock = response.results.filter(
+          (item) => item.orderedQuantity > item.remainingQuantity
+        );
+        const updatePromises = cartItemsOutOfStock.map((item) =>
+          editCartItemQuantity({
+            productVariantId: item.productVariantId,
+            quantity: item.remainingQuantity,
+            currentPrice: item.price,
+          })
+        );
+        await Promise.all(updatePromises);
+        const updatedResponse = await getAllProductsInCart();
+        if (updatedResponse) {
+          const newTotalCost = updatedResponse.results.reduce((total, item) => {
+            if (listItemChecked.includes(item.cartItemId)) {
+              return total + item.price * item.orderedQuantity;
+            }
+            return total;
+          }, 0);
+          setTotalCost(newTotalCost);
+          setProducts(updatedResponse.results);
+        }
+      }
     } catch (error) {
       console.error(error);
       toast.error("Failed to fetch products in cart");
@@ -286,14 +299,12 @@ const ShowProduct = () => {
               break;
           }
         }
-        setErrorQuantity((prev) => ({ ...prev, [cartItemId]: false }));
       }
     } catch (error) {
       console.error(error);
       const { code, message } = error.response.data;
       if (code === 400 && message === "Quantity exceeds available stock") {
-        setErrorQuantity((prev) => ({ ...prev, [data.cartItemId]: true }));
-        toast.error("Quantity exceeds available stock");
+        setOpenModal(true);
       } else toast.error("Failed to update quantity");
     }
   };
@@ -323,79 +334,130 @@ const ShowProduct = () => {
   );
 
   return (
-    <div className={css.containerShowProducts}>
-      <div className={css.leftPanel}>
-        <div className={css.headerRow}>
-          {label.map((item, index) => (
-            <div key={index} className={css.headerItem}>
-              {item}
+    <>
+      <div className={css.containerShowProducts}>
+        <div className={css.leftPanel}>
+          <div className={css.headerRow}>
+            {label.map((item, index) => (
+              <div key={index} className={css.headerItem}>
+                {item}
+              </div>
+            ))}
+          </div>
+          <div>
+            {paginatedProducts.map((item, index) => (
+              <CartItem
+                key={index}
+                item={item}
+                onCheckItem={onCheckItem}
+                listItemChecked={listItemChecked}
+                handleRemoveProduct={handleRemoveProduct}
+                handleChangeQuantityProduct={handleChangeQuantityProduct}
+              />
+            ))}
+          </div>
+          <div
+            style={{
+              display: "flex",
+              justifyContent: "center",
+              marginTop: "20px",
+            }}
+          >
+            {products && products.length > 0 && (
+              <Pagination
+                count={totalPages}
+                page={page}
+                onChange={handleChangePage}
+                color="black"
+                size="large"
+              />
+            )}
+          </div>
+          {paginatedProducts &&
+            paginatedProducts.length === 0 &&
+            products &&
+            products.length === 0 && (
+              <div className={css.emptyContainer}>
+                <div className={css.emptyTitle}>
+                  Your cart is currently empty. Browse our products and add
+                  items to your cart!
+                </div>
+                <div>
+                  <Link to="/">
+                    <button className={css.emptyButton}>Start shopping!</button>
+                  </Link>
+                </div>
+              </div>
+            )}
+        </div>
+        <div className={css.rightPanel}>
+          <div className={css.headerRightPanel}>Order information</div>
+          <div className={css.voucher}>
+            <div className={css.iconVoucher}>
+              <VoucherIcon />
+              <span>Voucher</span>
             </div>
-          ))}
+            <div className={css.voucherText}>Pick one</div>
+          </div>
+
+          <DeliveryOptions
+            selectedOption={selectedOption}
+            setSelectedOption={setSelectedOption}
+          />
+          <TotalCost totalCost={totalCost} deliveryCost={deliveryCost} />
         </div>
-        <div>
-          {paginatedProducts.map((item, index) => (
-            <CartItem
-              key={index}
-              item={item}
-              onCheckItem={onCheckItem}
-              listItemChecked={listItemChecked}
-              handleRemoveProduct={handleRemoveProduct}
-              handleChangeQuantityProduct={handleChangeQuantityProduct}
-              errorQuantity={errorQuantity[item.cartItemId]}
-            />
-          ))}
-        </div>
-        <div
-          style={{
-            display: "flex",
-            justifyContent: "center",
-            marginTop: "20px",
+      </div>
+      <Modal
+        open={openModal}
+        onClose={() => setOpenModal(false)}
+        aria-labelledby="modal-title"
+        aria-describedby="modal-description"
+      >
+        <Box
+          sx={{
+            position: "absolute",
+            top: "50%",
+            left: "50%",
+            transform: "translate(-50%, -50%)",
+            width: 350,
+            bgcolor: "background.paper",
+            borderRadius: 1,
+            boxShadow: 24,
+            p: 3,
           }}
         >
-          {products && products.length > 0 && (
-            <Pagination
-              count={totalPages}
-              page={page}
-              onChange={handleChangePage}
-              color="black"
-              size="large"
-            />
-          )}
-        </div>
-        {paginatedProducts &&
-          paginatedProducts.length === 0 &&
-          products &&
-          products.length === 0 && (
-            <div className={css.emptyContainer}>
-              <div className={css.emptyTitle}>
-                Your cart is currently empty. Browse our products and add items
-                to your cart!
-              </div>
-              <div>
-                <Link to="/">
-                  <button className={css.emptyButton}>Start shopping!</button>
-                </Link>
-              </div>
-            </div>
-          )}
-      </div>
-      <div className={css.rightPanel}>
-        <div className={css.headerRightPanel}>Order information</div>
-        <div className={css.voucher}>
-          <div className={css.iconVoucher}>
-            <VoucherIcon />
-            <span>Voucher</span>
-          </div>
-          <div className={css.voucherText}>Pick one</div>
-        </div>
-
-        <DeliveryOptions
-          selectedOption={selectedOption}
-          setSelectedOption={setSelectedOption}
-        />
-        <TotalCost totalCost={totalCost} deliveryCost={deliveryCost} />
-      </div>
-    </div>
+          <Typography id="modal-title" variant="h6" component="h2">
+            Out of Stock
+          </Typography>
+          <Typography id="modal-description" sx={{ mt: 1 }}>
+            The quantity you requested exceeds the available stock.
+          </Typography>
+          <Button
+            onClick={async () => {
+              await fetchProducts();
+              setOpenModal(false);
+            }}
+            variant="contained"
+            sx={{ mt: 2 }}
+            fullWidth
+            style={{
+              padding: "6px",
+              backgroundColor: "#274c50",
+              color: "#ffffff",
+              fontSize: "16px",
+              fontWeight: "bold",
+              border: "none",
+              borderRadius: "8px",
+              cursor: "pointer",
+              marginTop: "16px",
+              transition: "background-color 0.3s",
+            }}
+          >
+            OK
+          </Button>
+        </Box>
+      </Modal>
+    </>
   );
 };
 
